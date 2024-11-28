8171e4ede17dc748b160280f6191e8e4dd599ca6
maclaraoli <mariabacaia.0502@gmail.com>
2024-11-28 20:19:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Azul',
+    'ANUNCIADORES',
+    'Proclamadores da boa nova a todos os pobres de espírito.'
+)
+criaCartao(
+    'Vermelho',
+    'SERVOS',
+    'Oferecidos totalmente aos pobres, marginalizados, excluídos e abandonados.'
+)
+
+criaCartao(
+    'Roxo',
+    'VÍTIMAS',
+    'Pessoas que oferecem as próprias "chagas" em Cristo como holocausto de amor para a salvação de todos.'
+)
+
+criaCartao(
+    'Amarelo',
+    'ADORADORES',
+    'Intercessores, aos pés da Eucaristia para o movimento e a humanidade.'
+)
+
+criaCartao(
+    'Verde',
+    'PASTORES',
+    'Clérigos e otiros líderes cristãos que se tornam mensageiros da misericórdia de Deus, nas ações evangelizadoras, movimentos e comunidades, numa atenção privilegiada às ovelhas desgarradas.'
+)
+
+criaCartao(
+    'Rosa',
+    'CONSTRUTORES DA PAZ',
+    'Pessoas que consagram o próprio compromisso nos campos social, politico e cultural para a edificação do reino de justiça, fraternidade e paz.'
+)
+
+criaCartao(
+    'Laranja',
+    'ARTISTAS',
+    'Se comprometem, por meio da arte, a revelar ao mundo a beleza, a harmonia e a ternura do coração de Deus.'
+)
+
